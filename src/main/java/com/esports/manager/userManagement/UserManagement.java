26c6cf03635fc709df5f93d599b0dbdeac38b72c
Daniel Mehlber <daniel.mehlber@gmail.com>
2022-04-25 11:30:51 +0200

    added hover animation to card login and registration

---
+++ src/main/java/com/esports/manager/userManagement/UserManagement.java
@@ -0,0 +1,15 @@
+package com.esports.manager.userManagement.logic;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * collection of methods that will be used in user management
+ *
+ * @author Daniel Mehlber,
+ */
+public class UserManagement {
+
+    private static Logger log = LogManager.getLogger(UserManagement.class);
+
+}
