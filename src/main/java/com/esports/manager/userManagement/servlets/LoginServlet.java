f365b9883d8fb82240526fa459d59279b2cab687
Ph1L42 <philippphan@gmail.com>
2022-05-01 13:21:53 +0200

    Update LoginServlet.java

--- src/main/java/com/esports/manager/userManagement/servlets/LoginServlet.java
+++ src/main/java/com/esports/manager/userManagement/servlets/LoginServlet.java
@@ -2,6 +2,7 @@ package com.esports.manager.userManagement.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.http.HttpConnectTimeoutException;
 import java.rmi.ServerException;
 import java.rmi.server.ServerCloneException;
 
@@ -10,6 +11,7 @@ import javax.sql.rowset.serial.SerialException;
 import com.esports.manager.userManagement.beans.LoginData;
 
 import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -33,11 +35,16 @@ public class LoginServlet extends HttpServlet{
         */
         //User username = getByUsername(username);
         //String userpassword = username.getPassword();
-            RequestDispatcher rd = request.getRequestDispatcher(path);
+            RequestDispatcher rd = request.getRequestDispatcher("path");
             //Bean creation
             LoginData form = new LoginData();
             form.setUsername(request.getParameter("username"));
             form.setPassword(request.getParameter("password"));
-            rd.forward(request, response);
+            try{
+              rd.forward(request, response);  
+            } catch (ServletException e){
+               
+            }
+            
    }
 }
