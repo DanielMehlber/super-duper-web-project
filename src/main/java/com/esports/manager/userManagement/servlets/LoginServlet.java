24c934383230da23ab1b4f3f32821deaf577146c
Ph1L42 <68051686+Ph1L42@users.noreply.github.com>
2022-05-06 00:09:25 +0200

    Update LoginServlet.java

--- src/main/java/com/esports/manager/userManagement/servlets/LoginServlet.java
+++ src/main/java/com/esports/manager/userManagement/servlets/LoginServlet.java
@@ -24,7 +24,7 @@ import jakarta.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/*TODO
+/**TODO
  *Login Servlet
  * 
  * @author Philipp Phan
