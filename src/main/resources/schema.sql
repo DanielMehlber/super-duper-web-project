8a3d2e136f93c0d9ff5b49ea4d9a28dd9daf4b23
Maximilian Rublik <maximilian.rublik@hotmail.de>
2022-05-06 17:37:48 +0200

    Add initial .sql script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,15 @@
 --------------------------------------------------
 -- Authors:
 --------------------------------------------------
+
+CREATE SCHEMA `eSportDS`;
+USE `eSportDS`;
+
+CREATE TABLE `user` (
+  `Username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Logo` blob,
+  `ID` bigint NOT NULL AUTO_INCREMENT,
+  `Password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
