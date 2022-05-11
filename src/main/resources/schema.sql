1814adb56e3c3bada925e34c0311dd4b23f4ced3
Daniel Mehlber <daniel.mehlber@gmail.com>
2022-05-11 17:01:39 +0200

    sql script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,12 @@
---------------------------------------------------
 -- Authors:
---------------------------------------------------
 
-CREATE SCHEMA `eSportDS`;
-USE `eSportDS`;
+-- CREATE SCHEMA `eSportDS`;
+-- USE `eSportDS`;
 
 CREATE TABLE `user` (
-  `Username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Logo` blob,
-  `ID` bigint NOT NULL AUTO_INCREMENT,
-  `Password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+   `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `logo` blob,
+  `passwordHash` varchar(64) NOT NULL,
+)
