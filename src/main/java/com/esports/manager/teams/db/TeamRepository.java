7d40926076d052a914de17c2309dff55e0bc7193
Maximilian Rublik <maximilian.rublik@hotmail.de>
2022-05-23 01:30:45 +0200

    Add load team by id method and load team image methods

--- src/main/java/com/esports/manager/teams/db/TeamRepository.java
+++ src/main/java/com/esports/manager/teams/db/TeamRepository.java
@@ -6,6 +6,7 @@ import com.esports.manager.global.exceptions.InternalErrorException;
 import com.esports.manager.teams.entities.Team;
 import com.esports.manager.teams.exceptions.NoTeamsFoundException;
 
+import com.esports.manager.userManagement.exceptions.NoImageFoundException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,6 +21,7 @@ import java.util.List;
  * @author Maximilian Rublik
  */
 public class TeamRepository {
+    private static final Logger log = LogManager.getLogger(TeamRepository.class);
 
     /**
      * fetch all teams from database
@@ -28,14 +30,12 @@ public class TeamRepository {
      * @throws NoTeamsFoundException no teams found in database
      * @author Maximilian Rublik
      */
-    private static final Logger log = LogManager.getLogger(TeamRepository.class);
-
     public static List<Team> getAllTeams() throws InternalErrorException, NoTeamsFoundException {
         log.debug("fetching all team entities from database...");
 
         ResultSet resultSet;
         try {
-            PreparedStatement pstmt = QueryHandler.loadStatement("/sql/memberArea/fetchAllTeams.sql");
+            PreparedStatement pstmt = QueryHandler.loadStatement("/sql/teams/fetchAllTeams.sql");
             resultSet = pstmt.executeQuery();
         } catch (IOException | SQLException e) {
             log.error("cannot fetch teams from database because of an unexpected and fatal error:" + e.getMessage());
@@ -50,6 +50,96 @@ public class TeamRepository {
         }
 
         return teams;
+    }
+
+    public static Team getTeamById(final long id) throws InternalErrorException {
+        log.debug("fetch single team entity by id from database...");
+
+        ResultSet resultSet;
+        try {
+            PreparedStatement pstmt = QueryHandler.loadStatement("/sql/teams/fetchTeamById.sql");
+            pstmt.setLong(1, id);
+            resultSet = pstmt.executeQuery();
+        } catch (IOException | SQLException  e) {
+            log.error("cannot fetch single team by Id from database");
+            throw new InternalErrorException("cannot fetch Team by Id from database");
+        }
+
+        // Since we give back a List of results and we expect only a single one, we take the first one
+        Team team = ResultSetProcessor.convert(Team.class, resultSet).get(0);
+        return team;
+    }
+
+    /**
+     * Fetch team profile image
+     * @param id from team
+     * @return profile image from team
+     * @throws InternalErrorException some unexpected and fatal internal error occured
+     * @throws NoImageFoundException no image found to team
+     */
+    public static byte [] loadProfileImage(final Long id) throws InternalErrorException, NoImageFoundException {
+        log.debug("loading team profile image");
+
+        byte[] image;
+        try {
+            PreparedStatement pstmt = QueryHandler.loadStatement("/sql/teams/fetchTeamProfileImage.sql");
+            pstmt.setString(1, id.toString());
+            ResultSet result = pstmt.executeQuery();
+
+            if(result.next()) {
+                image = result.getBytes(1);
+            } else {
+                log.warn("no profile picture found for username");
+                throw new NoImageFoundException(id.toString(), "profile");
+            }
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        } catch (RuntimeException e) {
+            log.error("cannot fetch profile picture because of an unexpected internal error: " + e.getMessage());
+            throw new InternalErrorException("cannot fetch profile image", e);
+        }
+
+        if(image == null) {
+            log.warn("user " + id + " has no profile image");
+            throw new NoImageFoundException(id.toString(), "profile");
+        }
+
+        return image;
+    }
+
+    /**
+     * Fetch team background image
+     * @param id from team
+     * @return profile image from team
+     * @throws InternalErrorException some unexpected and fatal internal error occured
+     * @throws NoImageFoundException no image found to team
+     */
+    public static byte[] loadBackgroundImage(final Long id) throws InternalErrorException, NoImageFoundException {
+        log.debug("loading team background image");
+
+        byte[] image;
+        try {
+            PreparedStatement pstmt = QueryHandler.loadStatement("/sql/teams/fetchTeamBackgroundImage.sql");
+            pstmt.setString(1, id.toString());
+            ResultSet result = pstmt.executeQuery();
+
+            if (result.next()) {
+                image = result.getBytes(1);
+            } else {
+                throw new NoImageFoundException(id.toString(), "background");
+            }
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        } catch (RuntimeException e) {
+            throw new InternalErrorException("cannot fetch background image", e);
+        }
+
+        if (image == null) {
+            log.warn("team '" + id + "' has no background image");
+            throw new NoImageFoundException(id.toString(), "background");
+        }
+
+        return image;
     }
 
     /**
