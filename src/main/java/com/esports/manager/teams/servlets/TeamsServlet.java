d7a381601d5aa89d2147e92d5a103aea03602f08
Maximilian Rublik <maximilian.rublik@hotmail.de>
2022-05-23 01:34:06 +0200

    Clean up imports

--- src/main/java/com/esports/manager/teams/servlets/TeamsServlet.java
+++ src/main/java/com/esports/manager/teams/servlets/TeamsServlet.java
@@ -1,23 +1,20 @@
 package com.esports.manager.teams.servlets;
 
 import com.esports.manager.teams.beans.TeamsViewBean;
-import com.esports.manager.teams.entities.Team;
 import com.esports.manager.teams.exceptions.NoTeamsFoundException;
 import com.esports.manager.teams.logic.Teams;
+
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet("/teams")
 public class TeamsServlet extends HttpServlet {
@@ -27,7 +24,6 @@ public class TeamsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // TODO DO STUFF
         TeamsViewBean teamsViewBean = new TeamsViewBean();
-
 
         try {
             teamsViewBean.setTeams(Teams.fetchAllTeams());
