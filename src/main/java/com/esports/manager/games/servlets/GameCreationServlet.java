3944c3a8f2a73178d049b91354fc4dc6ab3d6ef9
Daniel Mehlber <40431741+DanielMehlber@users.noreply.github.com>
2022-06-21 21:51:01 +0200

    game search page and creation (#78)
    * game search page and creation
    
    * schema fix

---
+++ src/main/java/com/esports/manager/games/servlets/GameCreationServlet.java
@@ -0,0 +1,38 @@
+package com.esports.manager.games.servlets;
+
+import com.esports.manager.games.Games;
+import com.esports.manager.games.entities.Game;
+import com.esports.manager.userManagement.UserManagement;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+@WebServlet("/games/new")
+public class GameCreationServlet extends HttpServlet {
+
+    private final Logger log = LogManager.getLogger(GameCreationServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserManagement.getAuthorizedUser(req.getSession());
+
+        String titleParameter = req.getParameter("title");
+        if(titleParameter == null || titleParameter.isBlank()) {
+            log.warn("cannot create game: no title parameter provided");
+            resp.getWriter().println("no title provided");
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        Game game = Games.createGame(titleParameter, "");
+
+        // redirect
+        resp.sendRedirect(getServletContext().getContextPath() + "/games/game?id="+game.getId());
+    }
+}
--- src/webapp/jsp/fragments/create-game-modal.jsp
+++
@@ -1,9 +0,0 @@
-<script src="${pageContext.request.contextPath}/js/game-modal.js"></script>
-<section id="game-creation-modal" class="game-creation-modal-container">
-    <form class="game-creation-modal">
-        <h2>Create new Game</h2>
-        <input type="text" placeholder="Name of new Game"/>
-        <button type="submit">Send</button>
-        <div class="game-creation-modal-close" onclick="closeModal()">X</div>
-    </form>
-</section>
