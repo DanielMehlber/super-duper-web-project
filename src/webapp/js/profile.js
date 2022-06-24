bd398e87dedbe32fd97a2ad52adccd120f6bb528
Ph1L42 <68051686+Ph1L42@users.noreply.github.com>
2022-06-25 00:22:34 +0200

    Updated profilADI ADI ADI OH BVB HURENSÖHNE

---
+++ src/webapp/js/profile.js
@@ -0,0 +1,11 @@
+// Authors: Philipp Phan
+
+function updateBackgroundImage() {
+    const form = document.getElementById("player-background-image");
+    form.submit()
+}
+
+function updateProfileImage() {
+    const form = document.getElementById("profile-profile-image");
+    form.submit()
+}
