f34855f1321948145dfb2babbc8a3a711c7fdca2
Ph1L42 <68051686+Ph1L42@users.noreply.github.com>
2022-07-01 19:06:18 +0200

    Clean up LoginServlet

--- src/main/java/com/esports/manager/userManagement/servlets/LoginServlet.java
+++ src/main/java/com/esports/manager/userManagement/servlets/LoginServlet.java
@@ -27,12 +27,6 @@ import java.rmi.ServerException;
 public class LoginServlet extends HttpServlet {
     private static final Logger log = LogManager.getLogger(LoginServlet.class);
 
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/jsp/login.jsp").forward(req, resp);
-    }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServerException, IOException, ServletException {
