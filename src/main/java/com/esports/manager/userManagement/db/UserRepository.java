7d491005e6ad4bb5f5825836dff5ea0ac860a713
Ph1L42 <68051686+Ph1L42@users.noreply.github.com>
2022-07-01 11:41:17 +0200

    Updated Servlet

--- src/main/java/com/esports/manager/userManagement/db/UserRepository.java
+++ src/main/java/com/esports/manager/userManagement/db/UserRepository.java
@@ -265,10 +265,11 @@ public class UserRepository {
      *
      * @param username
      * @throws InternalErrorException
-     * @author Philipp Phan (Copy from V.Stiehl)
+     * @author Philipp Phan (Inspiration from V.Stiehl)
      */
     public static void deleteUser(String username) throws InternalErrorException{
         // Connect with Database and loads sql statement
+        // V.Siehl Page.18 (04_JDBC)
         try (PreparedStatement pstmt = QueryHandler.loadStatement("/sql/user-management/removeUser.sql");
              Connection connection = pstmt.getConnection()) {
             pstmt.setString(1, username);
